9697d67a4f198664cf43ebb057bff348656517a9
gs22111994 <gs22111994@gmail.com>
2020-03-23 17:27:47 +0530

    upload file in azure blob

--- QueueListener.cs
+++ QueueListener.cs
@@ -1,16 +1,23 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace QueueTrigger
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([QueueTrigger("todos", Connection = "")]string myQueueItem, ILogger log)
+        public static async Task Run([QueueTrigger("todos", Connection = "")]string myQueueItem,
+             [Blob("todos", Connection = "AzureWebJobsStorage")] CloudBlobContainer container,
+             ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            await container.CreateIfNotExistsAsync();
+            var blob = container.GetBlockBlobReference($"{myQueueItem}.txt");
+            await blob.UploadTextAsync($"Created a new task: {myQueueItem}");
         }
     }
 }
